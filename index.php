92991e847c39aae4ca1f786402f92232ae2b3d90
Partha Singha Roy <parthasingharoy166@gmail.com>
2022-02-08 20:13:57 +0530

    ok

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $connect = mysqli_connect($serverName, $userName, $pass,$DB);
 
 // Die if connection was not successfull
 if ($connect) {
-    echo 'Database is connected !!';
+    // echo 'Database is connected !!';
 } else {
     // echo alert-->
     echo 'Error !!';
@@ -38,46 +38,7 @@ if ($connect) {
     <title>PHP-CRUD-Create-Repeat-Update-Delete</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#"
-          ><img src="images/php-logo.png" width="40px" alt="php"
-        /></a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link">Contact Us</a>
-            </li>
-          </ul>
-          <form class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-light" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
+<?php include 'pages/nav.php' ?>
 
     <!-- form -->
 
@@ -97,7 +58,12 @@ if ($connect) {
     </div>
     <div class="container">
       <?php
-       
+       $sql = "SELECT * FROM `notes`";
+       $result = mysqli_query($connect,$sql);
+
+       while($note = mysqli_fetch_assoc($result)){
+         echo $note['SL.NO'].'hello'.$note['title'].'Hi'.$note['description'].'.';
+       }
 
        ?>
 
