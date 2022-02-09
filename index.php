5483e3529aadd36bdbb7aa16ffde2d056a74807c
Partha Singha Roy <parthasingharoy166@gmail.com>
2022-02-09 11:10:47 +0530

    update2

--- index.php
+++ index.php
@@ -210,21 +210,19 @@ if ($delete){
                  <td><button class='edit btn btn-primary btn-sm' id=".$note['SL.NO'].">Edit</button>  <button id=".$note['SL.NO']." class='delete btn btn-sm btn-primary' >delete</button> </td>
                </tr>";
                $SL=$SL+1;
-              // echo $note['SL.NO'].' hello '.$note['title'].' Hi '.$note['description'].' . <br>';
-
             }
             ?>
-
-
       </tbody>
     </table>
   </div>
+
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-    crossorigin="anonymous"></script>
+    crossorigin="anonymous">
+  </script>
 
-
+// for 
   <script>
     $(document).ready(function () {
       $('#myTable').DataTable();
